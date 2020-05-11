73788df32791fd83dd25614ad045b7d7221cc966
Ezequiel Hernán Villanueva <ezequiel.villanueva@gmail.com>
2020-05-11 08:13:37 -0300

    Add files via upload

---
+++ Ejemplo3.php
@@ -0,0 +1,16 @@
+<?
+$titulo="Este es el título de la página";
+$mensaje="Estamos realizando nuestra segunda página en PHP, y este es un comentario";
+?>
+
+<html>
+
+<head>
+	<title><? echo $titulo; ?></title>
+</head>
+
+<body>
+	<p><? echo $mensaje; ?></p>
+</body>
+
+</html>
