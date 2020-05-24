ceef067bfa51bb2d26af95a1e13ea942701e228d
Ezequiel Hernán Villanueva <ezequiel.villanueva@gmail.com>
2020-05-24 18:59:03 -0300

    Update Ejemplo4.php

--- Ejemplo4.php
+++ Ejemplo4.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 $titulo="Este es el título de la página";
 $nombre="Martín Sebastián Ramos Monso";
@@ -12,22 +12,22 @@ $numero=8;
 <html>
 
 <head>
-	<title><? echo $titulo; ?></title>
+	<title><?php echo $titulo; ?></title>
 </head>
 
 <body>
 	<table border="0" width="100%">
 		<tr>
-			<td width="100%">Mi nombre es: <b><? echo $nombre; ?></b></td>
+			<td width="100%">Mi nombre es: <b><?php echo $nombre; ?></b></td>
 		</tr>
 		<tr>
-			<td width="100%">Mi día preferido es el: <b><font color="#FF0000"><? echo $dia; ?></font></b></td>
+			<td width="100%">Mi día preferido es el: <b><font color="#FF0000"><?php echo $dia; ?></font></b></td>
 		</tr>
 		<tr>
-			<td width="100%">Vivo en: <b><i><? echo $vivo; ?></i></b></td>
+			<td width="100%">Vivo en: <b><i><?php echo $vivo; ?></i></b></td>
 		</tr>
 		<tr>
-			<td width="100%">Mi número de la suerte es: <u><font size="5"><? echo $numero; ?></font></u></td>
+			<td width="100%">Mi número de la suerte es: <u><font size="5"><?php echo $numero; ?></font></u></td>
 		</tr>
 	</table>
 </body>
