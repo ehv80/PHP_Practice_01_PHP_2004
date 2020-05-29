a864e9eceb21accb81e01c4e14f380cc035428ca
Ezequiel Hernán Villanueva <ezequiel.villanueva@gmail.com>
2020-05-29 20:59:52 -0300

    Add files via upload

---
+++ Ejemplo12.php
@@ -0,0 +1,12 @@
+<?php
+
+// Asigno valores a las variables 
+
+$articulo = "Telefono Motorola 3456";
+$precio = 12;
+
+echo "<br>Articulo es una variable del Tipo: ".gettype($articulo);
+
+echo "<br>Precio es una variable del Tipo: ".gettype($precio);
+
+?>
