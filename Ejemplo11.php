79df6e6f2d5854f5e38c37de2aa44e4412dd4f3d
Ezequiel Hernán Villanueva <ezequiel.villanueva@gmail.com>
2020-05-28 23:01:28 -0300

    Add files via upload

---
+++ Ejemplo11.php
@@ -0,0 +1,15 @@
+<?php
+
+// Comentarios 
+
+// Declaración de Constantes
+
+define("porcentaje",15);	// 15 lleva comillas, porque quiero que sea un valor entero, no texto
+
+define("nombre","MP Ediciones");
+
+echo $porcentaje;
+
+echo $nombre;
+
+?>
