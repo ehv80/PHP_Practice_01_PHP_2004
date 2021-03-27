689e096f92b606e8c0869c90b31762e584e47271
Ezequiel Hernán Villanueva <ezequiel.villanueva@gmail.com>
2021-03-27 20:37:25 -0300

    Add files via upload

---
+++ Ejemplo18.php
@@ -0,0 +1,16 @@
+<?php 
+//Ejemplo18.php Reworked
+
+// Asigno valores a las variables
+$precio = 12.5;
+
+if(isset($precio))
+{
+	echo "La variable precio está definida <br>";
+}
+else
+{
+	echo "La variable precio NO ESTÁ DEFINIDA <br>";
+}
+
+?>
