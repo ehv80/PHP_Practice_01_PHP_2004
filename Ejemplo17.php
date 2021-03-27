c25cbe7893319b32e2b7fd89dcaae08cf1fa7615
Ezequiel Hernán Villanueva <ezequiel.villanueva@gmail.com>
2021-03-27 20:09:14 -0300

    Add files via upload

---
+++ Ejemplo17.php
@@ -0,0 +1,16 @@
+<?php 
+//Ejemplo17.php Reworked
+
+// Asigno valores a las variables
+$precio = 12.5;
+
+if(is_double($precio))
+{
+	echo "El Tipo de Dato de la variable precio es: Double <br>";
+}
+else
+{
+	echo "El Tipo de Dato de la variable precio No Es Double <br>";
+}
+
+?>
