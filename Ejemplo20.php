b1df654cdae1916ab0a4650a75da361614b35f86
Ezequiel Hernán Villanueva <ezequiel.villanueva@gmail.com>
2021-03-27 21:31:15 -0300

    Add files via upload

---
+++ Ejemplo20.php
@@ -0,0 +1,31 @@
+<?php 
+//Ejemplo20.php ReWorked
+
+// Vamos a usar los operadores aritméticos
+
+// Asigno valores a las variables numéricas
+$a = 5;
+$b = 3;
+echo "La variable numérica a vale: $a y la otra b vale: $b <br>";
+
+// Sumamos los dos números y asignamos el resultado a otra variable
+$resultado = $a + $b;
+echo "La suma de $a + $b es: $resultado <br>";
+
+// Resta de dos números y asignamos el resultado a otra variable
+$resultado = $a - $b;
+echo "La resta $a - $b es: $resultado <br>";
+
+// Multiplicación de los 2 números y asignamos el resultado a otra variable
+$resultado = $a * $b;
+echo "La multiplicación $a * $b es: $resultado <br>";
+
+// División de los 2 números y asignamos el resultado a otra variable
+$resultado = $a / $b;
+echo "La división $a / $b es: $resultado <br>";
+
+// El Resto de la división de los 2 números y asignamos el resultado a otra variable
+$resultado = $a % $b;
+echo "El Resto de la división de $a / $b es: $resultado <br>";
+
+?>
