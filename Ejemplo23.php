b85fa0190db878dc51b677a0a5de4670313ba231
Ezequiel Hernán Villanueva <ezequiel.villanueva@gmail.com>
2021-03-30 19:32:55 -0300

    Add files via upload

---
+++ Ejemplo23.php
@@ -0,0 +1,30 @@
+<?php 
+//Ejemplo23.php ReWorked on English Language
+
+// The Use of the Increment & Decrement Operators
+
+// Inicialization of the variables
+$a = 5;
+echo "<br>Inicialization of the integer variable a that your value is: $a <br>";
+
+// Post-Increment of the variable a in one
+echo "<br>Post-Increment of the variable a in one with the post-increment operator that it's used like this: \$a++ <br>";
+$a++;
+echo "<br>Now the integer variable a have the value: $a <br>";
+
+// Pre-Increment of the variable a in one again
+echo "<br>Pre-Increment of the variable a in one again but with the pre-increment operator that it's used like this: ++\$a <br>";
+++$a;
+echo "<br>Now the integer variable a have the value: $a <br>";
+
+// Post-Decrement of the variable a in one
+echo "<br>Post-Decrement of the variable a in one with the post-decrement operator that it's used like this: \$a-- <br>";
+$a--;
+echo "<br>Now the integer variable a have the value: $a <br>";
+
+// Pre-Decrement of the variable a in one again
+echo "<br>Pre-Decrement of the variable a in one again but with the pre-decrement operator that it's used like this: --\$a <br>";
+--$a;
+echo "<br>Now the integer variable a have the value: $a <br>";
+
+?>
