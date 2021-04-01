4ff53d7ddb5ba74a74e9b22d0088e85200372f04
Ezequiel Hernán Villanueva <ezequiel.villanueva@gmail.com>
2021-03-31 21:53:17 -0300

    Add files via upload

---
+++ Ejemplo25.php
@@ -0,0 +1,17 @@
+<?php 
+//Ejemplo25.php ReWorked on English Language
+// Error Handling
+
+// Inicialization of the variables
+//$a = 5;
+//$result = 0;
+
+// This mathematics operation show an error Because Divide by Zero is impossible
+
+echo "<br>This operation show an error because Divide by Zero is impossible: \$result = 5/0 <br>";
+
+//$result = $a/0;
+$result = 5/0;
+
+echo "<br>The variable result have the value: $result <br>";
+?>
