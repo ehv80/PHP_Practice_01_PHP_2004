94b75db7e8f4533b1cd672e6753be342aefb2829
Ezequiel Hernán Villanueva <ezequiel.villanueva@gmail.com>
2021-04-01 11:50:01 -0300

    Add files via upload

---
+++ Ejemplo27.php
@@ -0,0 +1,55 @@
+<?php 
+// Ejemplo27.php ReWorked on English Language
+// Conditional Sentences
+
+// Inicialization of the variables
+
+$month = 1;
+echo "<br>Now the integer month variable have the value: $month <br>";
+if($month == 1) echo ("The number of the month variable correspond to January <br> <br>");
+
+$month = 2;
+echo "<br>Now the integer month variable have the value: $month <br>";
+if($month == 2) echo ("The number of the month variable correspond to February <br> <br>");
+
+$month = 3;
+echo "<br>Now the integer month variable have the value: $month <br>";
+if($month == 3) echo ("The number of the month variable correspond to March <br> <br>");
+
+$month = 4;
+echo "<br>Now the integer month variable have the value: $month <br>";
+if($month == 4) echo ("The number of the month variable correspond to April <br> <br>");
+
+$month = 5;
+echo "<br>Now the integer month variable have the value: $month <br>";
+if($month == 5) echo ("The number of the month variable correspond to May <br> <br>");
+
+$month = 6;
+echo "<br>Now the integer month variable have the value: $month <br>";
+if($month == 6) echo ("The number of the month variable correspond to June <br> <br>");
+
+$month = 7;
+echo "<br>Now the integer month variable have the value: $month <br>";
+if($month == 7) echo ("The number of the month variable correspond to July <br> <br>");
+
+$month = 8;
+echo "<br>Now the integer month variable have the value: $month <br>";
+if($month == 8) echo ("The number of the month variable correspond to August <br> <br>");
+
+$month = 9;
+echo "<br>Now the integer month variable have the value: $month <br>";
+if($month == 9) echo ("The number of the month variable correspond to September <br> <br>");
+
+$month = 10;
+echo "<br>Now the integer month variable have the value: $month <br>";
+if($month == 10) echo ("The number of the month variable correspond to October <br> <br>");
+
+$month = 11;
+echo "<br>Now the integer month variable have the value: $month <br>";
+if($month == 11) echo ("The number of the month variable correspond to November <br> <br>");
+
+$month = 12;
+echo "<br>Now the integer month variable have the value: $month <br>";
+if($month == 12) echo ("The number of the month variable correspond to December <br> <br>");
+
+?>
