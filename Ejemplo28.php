bd443478b9cb1b17799e85127f3b862f35eff00a
Ezequiel Hernán Villanueva <ezequiel.villanueva@gmail.com>
2021-04-01 19:40:51 -0300

    Add files via upload

---
+++ Ejemplo28.php
@@ -0,0 +1,15 @@
+<?php 
+// Ejemplo28.php ReWorked on English Language
+// Conditional Sentences
+
+// Inicialization of the variables
+
+$month = 1;
+echo "<br>Now the integer month variable have the value: $month <br>";
+if($month == 1)
+{
+	echo "<br>The number of the month variable correspond to January <br>";
+	echo "<br>And This month is calurous <br>";
+}
+
+?>
