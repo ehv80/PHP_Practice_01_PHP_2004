7dc62c460a0cd3a1c08fc1a06a9741426832d0f6
Ezequiel Hernán Villanueva <ezequiel.villanueva@gmail.com>
2021-05-24 10:23:44 -0300

    Add files via upload

---
+++ Ejemplo31.php
@@ -0,0 +1,32 @@
+<?php 
+/* 
+ * Ejemplo31.php / Example31.php
+ * 
+ * Vamos a ver otra forma de utilizar la sentencia condicional if pero sin las llaves, no es lo más recomendado.
+ * We will see another way to use the conditional if statement but withouth the braces, there is not the most recomended. 
+ * 
+ * Some source code files with examples from The Book
+ * "PROGRAMACIÓN PHP"
+ * a Interpreted Programming Language
+ * PHP Hipertext Preprocessor
+ * for various GNU\Linux based Operative Systems.
+ * Thanks & Writted by: Martín Ramos Monso @ REDUSERS
+ *
+ * Edited by Me a little time ago!
+ * 
+ * You can Copy / Edit / Change / Distribute
+ * GNU/GPL License Software Version 3.0
+ *
+ * For Educational Purpose.
+ *
+ */
+// Definimos y asignamos variables 
+// We define and asign values to the variables
+$numero = 5;
+
+// En un if sin llaves se ejecuta la primer línea de código siguiente cuando la condición es verdadera
+// In an if without braces, the first line of code below is executed when the condition is true 
+if( $numero == 3 )
+       echo "El número es Tres"; 
+echo "El número No es Tres";
+?>
