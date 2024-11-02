4c2fee7bffa581caa6204ff1bdcb6b715b6b42e6
Ezequiel Hernán Villanueva <ezequiel.villanueva@gmail.com>
2024-11-02 10:29:39 -0300

    Add files via upload

---
+++ Ejemplo54.php
@@ -0,0 +1,25 @@
+<?
+
+// Arrays en PHP
+// Definiendo un array simple con los días de la semana
+
+// Inicializamos el array
+
+$dias[1] = "Lunes";
+$dias[2] = "Martes";
+$dias[3] = "Miércoles";
+$dias[4] = "Jueves";
+$dias[5] = "Viernes";
+$dias[6] = "Sábado";
+$dias[7] = "Domingo";
+
+// Mostramos los valores cargados en el array
+echo $dias[1] . "<br>";
+echo $dias[2] . "<br>";
+echo $dias[3] . "<br>";
+echo $dias[4] . "<br>";
+echo $dias[5] . "<br>";
+echo $dias[6] . "<br>";
+echo $dias[7] . "<br>";
+
+?>
