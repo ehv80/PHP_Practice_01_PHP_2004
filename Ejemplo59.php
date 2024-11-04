4acae26fd5b6989e54b3ab35e39245251ddbc1fe
Ezequiel Hernán Villanueva <ezequiel.villanueva@gmail.com>
2024-11-04 09:34:40 -0300

    Add files via upload

---
+++ Ejemplo59.php
@@ -0,0 +1,16 @@
+<?
+
+// Arrays en PHP
+// Contando elementos de un array
+
+// Defino e inicializo el array 
+$mi_array = array("Argentina", "Uruguay", "Mexico", "Chile");
+
+// Cuento los elementos del array mediante la función count()
+echo $mi_array[0] . "<br>";
+echo $mi_array[1] . "<br>";
+echo $mi_array[2] . "<br>";
+echo $mi_array[3] . "<br>";
+echo "El array tiene " . count($mi_array) . " elementos" . "<br>";
+
+?>
