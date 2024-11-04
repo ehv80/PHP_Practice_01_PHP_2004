93ee500dc0be08eb699712833cdf9b847167af98
Ezequiel Hernán Villanueva <ezequiel.villanueva@gmail.com>
2024-11-04 09:07:18 -0300

    Update Ejemplo56.php

--- Ejemplo56.php
+++ Ejemplo56.php
@@ -13,4 +13,12 @@ $mi_array[] = "Bolivia";
 // Como vemos en este caso, no es necesario colocar el número de índice 
 // ya que se asigna automáticamente para cada valor, comenzando desde cero.
 
+// Mostramos los valores cargados en el array
+echo $mi_array[0] . "<br>";
+echo $mi_array[1] . "<br>";
+echo $mi_array[2] . "<br>";
+echo $mi_array[3] . "<br>";
+echo $mi_array[4] . "<br>";
+echo $mi_array[5] . "<br>";
+
 ?>
