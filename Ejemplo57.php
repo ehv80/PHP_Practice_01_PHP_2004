f9b08528adf67db779f4440390a5d0557228ad6a
Ezequiel Hernán Villanueva <ezequiel.villanueva@gmail.com>
2024-11-04 09:15:46 -0300

    Add files via upload

---
+++ Ejemplo57.php
@@ -0,0 +1,16 @@
+<? 
+
+// Arrays en PHP
+// Inicializando arrays
+
+$mi_array[0] = "Argentina";
+$mi_array[1] = "Uruguay";
+$mi_array[2] = "Mexico";
+$mi_array[4] = "Chile";
+
+// Mostramos los valores cargados en el Array 
+echo $mi_array[0] . "<br>";
+echo $mi_array[1] . "<br>";
+echo $mi_array[2] . "<br>";
+echo $mi_array[4] . "<br>";
+?>
